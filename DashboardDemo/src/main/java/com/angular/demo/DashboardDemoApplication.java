bb98d98d9e17b3adcc7273bda52e9a2e0a029003
priyav6477 <priyav6477@gmail.com>
2022-12-02 14:56:35 +0530

    demo push

---
+++ DashboardDemo/src/main/java/com/angular/demo/DashboardDemoApplication.java
@@ -0,0 +1,13 @@
+package com.angular.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardDemoApplication.class, args);
+	}
+
+}
